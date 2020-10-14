4e39cad809588ee6a6e8576cc39b004a50db03f1
helloslytherin <metro-chris@ya.ru>
2020-10-14 16:33:36 +0300

    Попытка 12345

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var textButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        textButton.layer.cornerRadius = 10
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
     }
 
-
+    
+    @IBAction func textButtonPressed() {
+        if redLightView.alpha == 1 {
+            textButton.setTitle("NEXT", for: .normal)
+        }
+    }
+    
 }
 
